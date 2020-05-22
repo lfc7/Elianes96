2c2717b46e2eb7c2ae8b526a9a1a1dd636c9d265
oliv <lfc7@users.noreply.github.com>
2020-05-23 01:47:14 +0200

    Delete runWS.sh

--- websocket_to_serial/runWS.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-SERIALPORT="/dev/ttyACM0"
-#SERIALPORT="/dev/tty4"
-#socat -d -d pty,raw,echo=0 pty,raw,echo=0
-
-#socat -d -d pty,raw,echo=0 pty,raw,echo=0
-
-stty -echo -F "$SERIALPORT" 2500000
-./websocketd --port=8080 --staticdir=. ./WSpipe.sh
